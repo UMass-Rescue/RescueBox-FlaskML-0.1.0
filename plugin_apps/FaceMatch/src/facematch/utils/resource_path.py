5e69c1cf84806e38a9f7ff78c84670aea2e023b1
nb950 <foth2012@gmail.com>
2025-01-06 19:17:12 -0500

    plugin_apps changes to log better. and try pyinstaller --this did not work for facematch and deepfake image. plugin_apps add place holders for weights files that are too large add pipfile and lock for virtualenv , tested ok with py 3.11 and 3.12 only add more log for facematch and samples for deep fake video

--- plugin_apps/FaceMatch/src/facematch/utils/resource_path.py
+++ plugin_apps/FaceMatch/src/facematch/utils/resource_path.py
@@ -1,17 +1,31 @@
-import os
+import os, sys
+from src.facematch.utils.logger import log_info
 
 module_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(
     os.path.join(module_dir, os.pardir, os.pardir, os.pardir)
 )
 DATA_DIR = os.path.join(project_root, "resources")
-FACEMATCH_DIR = os.path.join(project_root, "src", "facematch")
+if getattr(sys, 'frozen', False):
+    log_info("debug in get resource_path")
+    #script_dir = sys._MEIPASS
+    #DATA_DIR = os.path.join(script_dir, "resources")
+    log_info(DATA_DIR)
+
 os.makedirs(DATA_DIR, exist_ok=True)
 
 
 def get_resource_path(filename):
+    DIR = os.path.join(DATA_DIR, os.path.dirname(filename))
+    os.makedirs(DIR, exist_ok=True)
+    log_info("debug resource path dir")
+    log_info(DIR)
     return os.path.join(DATA_DIR, filename)
 
 
 def get_config_path(filename):
-    return os.path.join(FACEMATCH_DIR, "config", filename)
+    p = os.path.join(module_dir, os.pardir)
+    if getattr(sys, 'frozen', False):
+        p = "."
+    log_info(filename)
+    return os.path.join(p, "config", filename)
