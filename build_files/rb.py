dee437464584d004a19317b42b15479b3bae3d3c
nb950 <foth2012@gmail.com>
2025-01-19 20:24:21 -0500

    add config json for servr specific params fix issues found on mac

--- build_files/rb.py
+++ build_files/rb.py
@@ -6,53 +6,50 @@ import logging
 from pathlib import Path
 import subprocess
 import psutil
-from taskkill import taskkill_force_pid_children ,taskkill_regex_rearch
+import json
+import platform
+if platform.system() == 'Windows':
+    from taskkill import taskkill_force_pid_children ,taskkill_regex_rearch
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
 
-model = {
-        "5000": "dfVideo",
-        "5005": "dfImage",
-        "5010": "facematch",
-        "5020": "audio",
-    }
+SERVER_URL="http://127.0.0.1"
+SERVER_API="/api/app_metadata"
 
-RBLOG= os.path.join(os.environ['APPDATA'], 'RescueBox-Desktop', 'logs')
-RBLOG_FILE = os.path.join(RBLOG, 'rb_py.log')
-logging.basicConfig(filename=RBLOG_FILE, level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 cwd = Path.cwd()
-#print(f'cwd is {cwd}')
-#logging.info("CWD is  %s", cwd)
-RBHOME= os.path.join(cwd,"resources", "assets", "rb_server")
-if not os.path.exists(RBHOME):
-    RBHOME = os.path.join(".", "resources","assets","rb_server")
-if not os.path.exists(os.path.join(RBHOME, "plugin_apps")):
-    logging.info("RBHOME plugin_apps not found  %s", RBHOME)
+RBHOME = cwd
+RBHOME = os.path.join(cwd,"resources", "assets", "rb_server")
+
+RBLOG = os.path.join(cwd)
+if platform.system() == 'Windows':
+    RBLOG = os.path.join(os.environ['APPDATA'], 'RescueBox-Desktop', 'logs')
 else:
-    logging.info("RBHOME is Good !")
+    RBLOG = os.path.join(cwd, "logs")
+    os.makedirs(RBLOG, exist_ok=True)
+
+
+# save process ids to procfile
 procfile = os.path.join( RBLOG, 'rb_process.txt')
 my_file = Path(procfile)
-# logging.info("file %s process exists= %s", procfile, my_file.is_file())
 
-def delete_all_pids() -> None:
+def delete_all_pids(proc_name) -> None:
     """kill all the pipenv.exe procs"""
-    # kill all the pipenv.ece procs
-    results = taskkill_regex_rearch(
-      dryrun=False,
-      kill_children=True,
-      force_kill=True,
-      title=".*",
-      windowtext=".*",
-      class_name=".*",
-      path="pipenv.*$",
-    )
-    logging.info("RB model Server killed ok %s", results)
+    # kill all the pipenv.ece procs in one go
+    if platform.system() == 'Windows':
+        results = taskkill_regex_rearch(
+        dryrun=False,
+        kill_children=True,
+        force_kill=True,
+        title=".*",
+        windowtext=".*",
+        class_name=".*",
+        path=f'{proc_name}' +"$",
+        )
+        logging.info("RB model Server killed ok %s", results)
     for proc in psutil.process_iter():
-          # print(f'found {str(proc.name), str(proc.pid) }')
-          if proc.name() == "pipenv.exe" :
+          if proc.name() == proc_name :
             children = proc.children(recursive=True)
             for child in children:
                 try:
@@ -62,10 +59,10 @@ def delete_all_pids() -> None:
             proc.kill()
             logging.info("RB model Server killed ok with cmdline %s",proc.name())
 
-def stop_existing_pids(spid):
+def stop_existing_pids(proc_name,spid):
     """kill existing pipenv.exe proc"""
     for proc in psutil.process_iter(['pid', 'name']):
-        if str(proc.info["name"]) != 'pipenv.exe':
+        if str(proc.info["name"]) != proc_name:
             continue
         pid = proc.info['pid']
         results = taskkill_force_pid_children(pid)
@@ -83,7 +80,7 @@ def stop_existing_pids(spid):
     return False
 
 def create_new_pids(model,new_pid):
-    """Create new server"""
+    """Create new server pids on restart"""
     with open(procfile, 'r') as f:
         lines = f.readlines()
         logging.info("existing lines in process.txt %s", lines)
@@ -99,17 +96,14 @@ def create_new_pids(model,new_pid):
     with open(procfile, 'w') as fw:
         fw.writelines(out_lines)
 
-def check_server_ok(port, retry,delay):
+def check_server_ok(port,backoff, retry, delay):
     """Check server rest api"""
-   # call rest api at this port
-   # Replace with the correct URL
-   # curl -lkiv http://127.0.0.1:5000/api/app_metadata
     try:
         http = requests.Session()
-        retries = Retry(total=retry, backoff_factor=10, status_forcelist=[429, 500, 502, 503, 504])
+        retries = Retry(total=retry, backoff_factor=backoff, status_forcelist=[429, 500, 502, 503, 504])
         http.mount("http://", HTTPAdapter(max_retries=retries))
 
-        url = "http://127.0.0.1" + ":" + port + "/api/app_metadata"
+        url = SERVER_URL + ":" + port + SERVER_API
         response = http.get(url, timeout=delay)
         logging.info("http resp %s",response)
         #response.raise_for_status()
@@ -133,59 +127,50 @@ def check_exists(pdir) -> bool:
         return False
     return True
 
-def restart_on_port(model_name, port) -> None:
+def restart_on_port(server, model_name, port) -> None:
     """Restart server on port """
-    # restart model and replace new pid
     # get cmdline to run process from pshell
-    servers = {}
-    pdir  = os.path.join(RBHOME, "plugin_apps","audio-transcription","audio_transcription")
-    start = f'pipenv run python {pdir}/server.py'
-    servers["5020"]=[pdir, start]
-    pdir  = os.path.join(RBHOME, "plugin_apps","FaceMatch")
-    start = "pipenv run python server.py"
-    servers["5010"]=[pdir, start]
-    pdir  = os.path.join(RBHOME, "plugin_apps","DeepFakeDetector","image_model")
-    start = "pipenv run python model_server.py"
-    servers["5005"]=[pdir, start]
-    pdir  = os.path.join(RBHOME, "plugin_apps","DeepFakeDetector","video_detector")
-    start = "pipenv run python server.py"
-    servers["5000"]=[pdir, start]
+    logging.info("restart %s on %s", model_name, port)
+    
+    p = server["pdir"].split(",")
+    pdir = os.path.join(*p)
+    pstart = server["pstart"]
 
-    serv = servers[port]
-    pdir = serv[0]
-    startp = serv[1]
     if not check_exists(pdir):
         print("Model Server not installed %s %s" , pdir)
         return
 
-    print(f'Starting Model Server {pdir} {startp}')
+    print(f'Starting Model Server {pdir} {pstart}')
     
-    logging.info("Starting Model Server %s %s", pdir, startp)
+    logging.info("Starting Model Server %s %s", pdir, pstart)
 
-    # start server f'cmd /c pipenv shell',
-    cmds = [f'cd {pdir} && pipenv sync && {startp}']
-    new_pid = ""
-    for c in cmds:
-        print(f'run cmd: {c}')
-        logging.info('run cmd %s', c)
-        process = subprocess.Popen(c, close_fds=False,shell=True,stderr=subprocess.STDOUT,creationflags=subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP)
-        new_pid = process.pid
-        print(f'run cmd pid  {new_pid}')
-        logging.info('pid %s', new_pid)
+    # start server cmds in one process
+    if platform.system() == 'Windows':
+        cmds = [f'cd {pdir} && pipenv sync && {pstart}']
+        new_pid = ""
+        for c in cmds:
+            print(f'run cmd: {c}')
+            logging.info('run cmd %s', c)
+            process = subprocess.Popen(c, close_fds=False,shell=True,stderr=subprocess.STDOUT,
+                                    creationflags=subprocess.DETACHED_PROCESS | 
+                                    subprocess.CREATE_NEW_PROCESS_GROUP)
+            new_pid = process.pid
+            print(f'run cmd pid  {new_pid}')
+            logging.info('pid %s', new_pid)
+    else:
+        logging.info('process start not implemented on mac /linux ')
+        return
 
 
-    # process = subprocess.Popen(["pipenv", "run", "python", "server.py"],
-    #                           creationflags=subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP,close_fds=True,
-    #                           stdout=None, stderr=None)
-
-    # new_pid = process.pid
     print(f'Process new pid: {new_pid}')
     logging.info("Process %s new PID %s",model_name, new_pid)
-    rc = check_server_ok(port,3,3)
+    rc = check_server_ok(port,10,3,3)
     logging.info("check server return %s", rc)
     # replace pid in process.txt
     # done
     create_new_pids(model_name, new_pid)
+    logging.info("restart completed %s on %s", model_name, port)
+
 
 def find_and_stop_each_pid() -> None:
     if my_file.is_file():
@@ -200,39 +185,73 @@ def find_and_stop_each_pid() -> None:
               #print(f"found process {x[0]} port {x[1]}")
               logging.info("stop process %s %s", stop_pid[0], stop_pid[1])
               #logging.info(f"psutil {psutil}")
-              stop_existing_pids(stop_pid)
+              stop_existing_pids("pipenv.exe",stop_pid)
 
-def main() -> None:
-    """Main logic of the script"""
-    logging.info('This is rb start/stop utility ')
-    #logging.info("using log file %s", RBLOG_FILE)
+def find_and_stop_server_pids() ->None:
+    find_and_stop_each_pid()
+    # no other option kill all pids
+    try:
+        f = os.remove(procfile)
+        logging.info("remvove proc file fd=%s %s",f, procfile)
+    except Exception as e:
+        logging.info("remvove proc file %s", e)
+    try:
+        delete_all_pids("pipenv.exe")
+    except Exception as e:
+        logging.info("delete all pids done %s", e)
+
+def setup_logging():
+    '''Setup paths and log'''
+    RBHOME = cwd
+    if platform.system() == 'Windows':
+        RBHOME = os.path.join(cwd,"resources", "assets", "rb_server")
+
+    RBLOG_FILE = os.path.join(RBLOG, 'rb_py.log')
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+
+    logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s',
+                    handlers=[logging.FileHandler(filename=RBLOG_FILE, mode='w'),
+                              stream_handler])
+    if not os.path.exists(RBHOME):
+        RBHOME = os.path.join(".", "resources","assets","rb_server")
+    if not os.path.exists(os.path.join(RBHOME, "plugin_apps")):
+        logging.info("RBHOME plugin_apps not found  %s", RBHOME)
+    else:
+        logging.info("RBHOME is Good !")
+
+def parse_args() -> dict:
     parser = argparse.ArgumentParser(description="Run a server.")
     parser.add_argument(
             "--port", type=str, help="Port number to run the server"
         )
-    args = parser.parse_args()
-    port=args.port
-    logging.info("port given %s", port)
-    # either kill one one by one to be safe
-    if port is None:
-        find_and_stop_each_pid()
-        # no other option kill all pids
-        try:
-            f = os.remove(procfile)
-            logging.info("remvove proc file fd=%s %s",f, procfile)
-        except Exception as e:
-            logging.info("remvove proc file %s", e)
-        try:
-          delete_all_pids()
-        except Exception as e:
-          logging.info("delete all pids done %s", e)
+    args = parser.parse_args(namespace=None)
+    return args
 
-    model_name = model.get(port)
-    if model_name is not None and not check_server_ok(port, 1, .1):
-        print(f'restart {model_name} on {port}')
-        logging.info("restart %s on %s", model_name, port)
-        restart_on_port(model_name, port)
-        logging.info("restart completed %s on %s", model_name, port)
+def main() -> None:
+    """Main logic of the script"""
+    setup_logging()
+    logging.info('This is rb start/stop utility ')
+
+    # Read mode server config from a file
+    server = {}
+    with open('server_config.json', 'r') as f:
+        models = json.load(f)
+        servers= models["servers"]
+        #logging.info("server given %s", servers)
+
+    #logging.info("using log file %s", RBLOG_FILE)
+    args=parse_args()
+    logging.info("port given %s", args.port)
+    # either kill one one by one to be safe
+    if args.port is None:
+        find_and_stop_server_pids()
+
+    logging.info("model found %s", servers[args.port]["pname"])
+    model_name = servers[args.port]["pname"]
+    if model_name is not None and not check_server_ok(args.port, 10, 1, .1):
+        restart_on_port(servers[args.port], model_name, args.port)
 
 if __name__ == "__main__":
     main()
